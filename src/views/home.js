78f2f3018e9ca76a765c0c62a1caf95d65cb1945
teledependent71 <93117032+teledependent71@users.noreply.github.com>
2024-01-23 20:46:19 +0200

    Commit from TeleportHQ

---
+++ src/views/home.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import { Helmet } from 'react-helmet'
+
+import './home.css'
+
+const Home = (props) => {
+  return (
+    <div className="home-container">
+      <Helmet>
+        <title>Central Implementation Orchestrator</title>
+        <meta
+          property="og:title"
+          content="Central Implementation Orchestrator"
+        />
+      </Helmet>
+    </div>
+  )
+}
+
+export default Home
